5509e989b5cf4f71bf3901e71e8380258e513541
adamwales1 <95204890+adamwales1@users.noreply.github.com>
2022-01-25 02:59:10 -0800

    Add files via upload
    Class exercises 23 to 24 Jan 22

---
+++ ipPhone.ps1
@@ -0,0 +1,16 @@
+﻿
+
+
+
+$Testusers = Get-ADGroupMember -Identity IPPhoneTest
+foreach ($User in $Testusers)  {
+    $Userinfo = Get-ADUser -Identity $User
+    $IPPhoneAttribute = $Userinfo.GivenName + '.' + $Userinfo.Surname + '@adatum.com'
+    Set-ADUser -Identity $user -Replace @{ipPhone = $IPPhoneAttribute}
+}
+
+
+
+Get-ADUser -Filter * -Properties * | where-object {$_.Name -eq "abbi skinner"}
+
+
