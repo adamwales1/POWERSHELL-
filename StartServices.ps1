d4fef9f4a9214690a038ce9408b01146d8a16ea2
adamwales1 <95204890+adamwales1@users.noreply.github.com>
2022-01-25 03:04:19 -0800

    Update StartServices.ps1

--- StartServices.ps1
+++ StartServices.ps1
@@ -1,1 +1,11 @@
-﻿
+
+
+$ServiceNames = Get-Content E:\services.txt
+foreach ($Service in $ServiceNames) {
+    $Serviceinfo = get-service -name $Service
+    if ($Serviceinfo.Status -eq 'Running') {write-host "$Service, No action required"}
+    else {
+        start-service -Name $Service
+        write-host "$Service, Service was started"
+    }
+}
